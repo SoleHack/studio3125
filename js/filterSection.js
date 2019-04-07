2896015ef59ca62ed62e3971d8df267c8704ff5f
Josh Anderson <anderson.joshuas89@gmail.com>
2019-04-07 00:48:27 -0700

    services updates
    Test html created.

--- js/filterSection.js
+++ js/filterSection.js
@@ -45,3 +45,16 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
